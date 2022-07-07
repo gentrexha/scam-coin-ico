8347f945798efa852dac8a6eff7a719efef6dd91
Gent Rexha <gent@cow.fi>
2022-07-07 09:56:00 +0200

    Initialized repository

---
+++ README.md
@@ -0,0 +1,37 @@
+# Scam Coin ICO
+
+## Goal
+
+Write an ICO contract to raise 1000 WETH. Anyone can participate in the sale by transferring WETH (wrapped Ether) 
+to the ICO contract. Once the 1000 WETH are raised, the sale is over and no one can participate anymore. 
+2 minutes after the sale ended the participants can claim 10 SCAM tokens from the ICO contract for every WETH they invested.
+
+## Requirements
+
+- Write contracts in solidity.
+  - Write ICO contract based on upper requirements.
+  - Create SCAM token.
+    - Decimals: 18
+    - Name: Scam Coin
+    - Symbol: SCAM
+  - Use the WETH wrapped ether token (https://github.com/gnosis/canonical-weth) NOT Ether directly to participate in the ICO.
+- Use Hardhat (or Foundry) and ethers.js and use WETH9 as an external dependency.
+- Unit tests for your smart contract!
+- Use https://github.com/sc-forks/solidity-coverage to test coverage and reach 100% with automated truffle tests.
+- Deploy contract on rinkeby testnet using hardat deploy. 
+- Save artifacts in a git repository.
+- Use https://safe-relay.dev.gnosisdev.com/api/v1/gas-station/ to estimate gas costs for transactions.
+- Implementation time: 1-5 days.
+- Share the git repository with your mentor.
+
+## Questions
+
+1. What is gas, gas limit and gas price?
+2. What is a nonce?
+3. What is a transaction hash?
+4. How can you estimate gas costs and what is the issue with eth_estimateGas?
+5. What is the difference between a smart contract and a private key controlled account?
+
+## Answers
+
+TODO!
---
+++ contracts/ICO.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "./ScamCoin.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ICO {
+    // The token being sold
+    ScamCoin private _token;
+
+    // only accept WETH
+    // TODO: Hardcoded address in here? There should be a better alternative
+    IERC20 private WETH = IERC20(0xc778417E063141139Fce010982780140Aa0cD5Ab);
+    
+    // Address where funds are collected
+    address payable private _wallet;
+    
+    // How many tokens should be sold
+    uint256 private _cap = 1000;
+
+    // How many token units a buyer gets per wei.
+    // The rate is the conversion between wei and the smallest and indivisible token unit.
+    // So, if you are using a rate of 1 with a ERC20 token with 18 decimals called SCAM
+    // 1 wei will give you 1 unit, or 0.000000000000000001 SCAM.
+    uint256 private _rate = 10;
+
+    // Amount of wei raised
+    uint256 private _weiRaised;
+
+    // Addresses to store coins while crowdsale is open
+    mapping(address => uint256) private _balances;
+
+    // Has our crowdsale ended
+    bool private _ended;
+
+    // time-restrictions
+    uint256 private _endTime;
+    uint256 private _waitTime;
+
+    /**
+     * Event for token purchase logging
+     * @param purchaser who paid for the tokens
+     * @param value weis paid for purchase
+     * @param amount amount of tokens purchased
+     */
+    event TokensPurchased(address indexed purchaser, uint256 value, uint256 amount);
+    
+    /**
+     * Event for declaring Crowdsale as done
+     * @param endtime when did the crowdsale finish
+     */
+    event CrowdsaleDone(uint256 endtime);
+
+    event DebugLog(string msg);
+
+    /**
+     * @param rate Number of token units a buyer gets per wei
+     * @param wallet Address where collected funds will be forwarded to
+     * @param token Address of the token being sold
+     * @param cap The maximum amount of tokens in the Crowdsale
+     */
+    constructor (uint256 rate, address payable wallet, ScamCoin token, uint256 cap, uint256 waitTime) {
+        require(rate > 0, "Crowdsale: rate is 0");
+        require(wallet != address(0), "Crowdsale: wallet is the zero address");
+        require(address(token) != address(0), "Crowdsale: token is the zero address");
+        require(cap > 0, "Crowdsale: cap is 0");
+
+        _rate = rate;
+        _wallet = wallet;
+        _token = token;
+        _cap = cap;
+        _waitTime = waitTime;
+    }
+
+    /**
+     * @dev Checks whether the cap has been reached.
+     * @return Whether the cap was reached
+     */
+    function capReached() public view returns (bool) {
+        return _weiRaised >= _cap;
+    }
+
+    /**
+     * @dev low level token purchase ***DO NOT OVERRIDE***
+     * This function has a non-reentrancy guard, so it shouldn't be called by
+     * another `nonReentrant` function.
+     */
+    function buyTokens(uint256 weiAmount) public {
+        _preValidatePurchase(weiAmount);
+
+        // calculate token amount to be created
+        uint256 tokens = _getTokenAmount(weiAmount);
+
+        // update state
+        _weiRaised = _weiRaised + weiAmount;
+
+        // Check if we reached cap
+        if (_weiRaised == _cap) {
+            _ended = true;
+            _endTime = block.timestamp;
+            emit CrowdsaleDone(_endTime);
+        }
+
+        _processPurchase(msg.sender, tokens);
+        emit TokensPurchased(msg.sender, weiAmount, tokens);
+    }
+
+    /**
+     * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met.
+     * @param weiAmount Value in wei involved in the purchase
+     */
+    function _preValidatePurchase(uint256 weiAmount) internal {
+        require(weiAmount != 0, "ICO: amount is 0");
+        require((_weiRaised + weiAmount) <= _cap, "ICO: cap exceeded");
+        require(WETH.transferFrom(msg.sender, _wallet, weiAmount));
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+    }
+
+
+    /**
+     * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends
+     * its tokens.
+     * @param beneficiary Address performing the token purchase
+     * @param tokenAmount Number of tokens to be emitted
+     */
+    function _assignTokens(address beneficiary, uint256 tokenAmount) internal {
+        _balances[beneficiary] = _balances[beneficiary] + tokenAmount;
+    }
+
+    /**
+     * @dev Executed when a purchase has been validated and is ready to be executed. Doesn't necessarily emit/send
+     * tokens.
+     * @param beneficiary Address receiving the tokens
+     * @param tokenAmount Number of tokens to be purchased
+     */
+    function _processPurchase(address beneficiary, uint256 tokenAmount) internal {
+        _assignTokens(beneficiary, tokenAmount);
+    }
+
+
+    /**
+     * @dev Override to extend the way in which ether is converted to tokens.
+     * @param weiAmount Value in wei to be converted into tokens
+     * @return Number of tokens that can be purchased with the specified _weiAmount
+     */
+    function _getTokenAmount(uint256 weiAmount) internal view returns (uint256) {
+        return weiAmount * _rate;
+    }
+
+    /**
+     * @dev Withdraw tokens only after crowdsale ends.
+     * @param beneficiary Whose tokens will be withdrawn.
+     */
+    function withdrawTokens(address beneficiary) public {
+        require(_ended, "Crowdsale: not closed");
+        uint256 amount = _balances[beneficiary];
+        require(amount > 0, "Crowdsale: beneficiary is not due any tokens");
+
+        _balances[beneficiary] = 0;
+        _token.transferFrom(address(this), beneficiary, amount);
+    }
+
+    /**
+     * @return the balance of an account.
+     */
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+}
